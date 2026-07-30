738b6ef2d71f88465d170272517de0fb0828a787
FoXeN <ffoxene@gmail.com>
2026-07-30 19:05:46 +0300

    test: add ProfilePage object and fixture

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -1,10 +1,12 @@
 import { test as base } from '@playwright/test'
 import { LoginPage } from './pages/LoginPage'
 import { HomePage } from './pages/HomePage'
+import { ProfilePage } from './pages/ProfilePage'
 
 type MyFixtures = {
   loginPage: LoginPage,
-  homePage: HomePage
+  homePage: HomePage,
+  profilePage: ProfilePage
 }
 
 export const test = base.extend<MyFixtures>({
@@ -17,6 +19,11 @@ export const test = base.extend<MyFixtures>({
     const homePage = new HomePage(page)
     await homePage.goto()
     await use(homePage)
+  },
+  profilePage: async ({ page }, use) => {
+    const profigePage = new ProfilePage(page)
+    await profigePage.goto()
+    await use(profigePage)
   }
 })
 
---
+++ tests/pages/ProfilePage.ts
@@ -0,0 +1,20 @@
+import { Page, Locator } from '@playwright/test'
+
+export class ProfilePage {
+  readonly page: Page
+  readonly nameField: Locator
+  readonly surnameField: Locator
+  readonly emailField: Locator
+
+  constructor(page: Page) {
+    this.page = page
+    // в данном случае удобнее использовать регулярку, так как мы не знаем что идет после поля
+    this.nameField = page.getByText(/Name:\s\D+/)
+    this.surnameField = page.getByText(/Surname:\s\D+/)
+    this.emailField = page.getByText(/Email:\s\D+/)
+  }
+
+  async goto() {
+    await this.page.goto('/profile')
+  }
+}
